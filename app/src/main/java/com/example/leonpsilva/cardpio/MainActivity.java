1822bd081c1a41f8fed06221dbf1cde121ae20ca
LeonpSilva <leon-japa@hotmail.com>
2019-08-27 21:17:21 -0300

    27/08

--- app/src/main/java/com/example/leonpsilva/cardpio/MainActivity.java
+++ app/src/main/java/com/example/leonpsilva/cardpio/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.leonpsilva.cardpio;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.leonpsilva.cardpio.modelos.Produto;
@@ -15,6 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menuItem1) {
+            //Faz alguma coisa ao clicar no menu
+            Intent intent = new Intent(this, SobreActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void abrirLanche(View v) {
--- app/src/main/java/com/example/leonpsilva/cardpio/ProdutosActivity.java
+++ app/src/main/java/com/example/leonpsilva/cardpio/ProdutosActivity.java
@@ -3,6 +3,7 @@ package com.example.leonpsilva.cardpio;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -15,6 +16,7 @@ public class ProdutosActivity extends AppCompatActivity {
     private TextView textViewTitulo;
     private LinearLayout linearLayout;
     private ArrayList<Produto> produtos = new ArrayList<>();
+    private ImageView imageViewCategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class ProdutosActivity extends AppCompatActivity {
 
         textViewTitulo = findViewById(R.id.textViewTitulo);
         linearLayout = findViewById(R.id.layout);
+        imageViewCategoria = findViewById(R.id.imageViewCategoria);
 
         Intent intent = getIntent();
         String titulo = intent.getStringExtra(MainActivity.TITULO);
@@ -36,17 +39,34 @@ public class ProdutosActivity extends AppCompatActivity {
    public void criarProdutos(String titulo) {
 
         if (titulo.equals("Lanches")) {
+            imageViewCategoria.setImageResource(R.drawable.lanche);
             produtos.add(new Produto("Lanche de bacon",
-                    13.99, "Delicioso lanche de bacon",
+                    13.99, "Delicioso lanche de Bacon",
+                    "Lanche"));
+
+            produtos.add(new Produto("X-Costela",
+                    24.99,
+                    "Delicioso lanche de Costela",
+                    "Lanche"));
+
+            produtos.add(new Produto("X-Tudo",
+                    22.99,
+                    "Delioso lanche de Tudo",
                     "Lanche"));
 
         }else if (titulo.equals("Picanhas")) {
-
+            imageViewCategoria.setImageResource(R.drawable.picanha);
             produtos.add(new Produto("Picanha a bafo",
-                    99.99,"Deliciosa picanha a bafo",
+                    125.99,"Deliciosa picanha a bafo",
+                    "Picanhas"));
+
+            produtos.add(new Produto("Picanha Grelhada",
+                    70.99,
+                    "Picanha grelhada Suculenta",
                     "Picanhas"));
 
        }else if (titulo.equals("Pizzas")) {
+            imageViewCategoria.setImageResource(R.drawable.pizza);
             produtos.add(new Produto("Pizza de Calabresa",
                     20.99,
                     "Deliciosa Pizza crocante com gosto de calabresa",
@@ -57,9 +77,8 @@ public class ProdutosActivity extends AppCompatActivity {
                     "Deliciosa Pizza crocante com gosto de Queijo",
                     "Pizza"));
 
-
-
         }else if (titulo.equals("Quibes")) {
+            imageViewCategoria.setImageResource(R.drawable.quibe);
             produtos.add(new Produto("Quibe de Frango",
                     12.99,
                     "Delicioso quibe de frango grelhado",
@@ -73,14 +92,18 @@ public class ProdutosActivity extends AppCompatActivity {
         for (int i=0; i<lista.size();i++) {
             TextView textViewNome = new TextView(this);
             TextView textViewPreco = new TextView(this);
+            TextView textViewDescricao = new TextView(this);
 
             textViewNome.setText(lista.get(i).getNome());
             textViewNome.setTextSize(18);
 
-            textViewPreco.setText((lista.get(i).getPreco().toString()));
+            textViewPreco.setText("R$:" + lista.get(i).getPreco().toString().replace(".",","));
+
+            textViewDescricao.setText(lista.get(i).getDescricao());
 
             linearLayout.addView(textViewNome);
             linearLayout.addView(textViewPreco);
+            linearLayout.addView(textViewDescricao);
         }
    }
 
